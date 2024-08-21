15cd611af534d3187eb9d8ea6545b9883118fe9a
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-21 07:04:31 +0200

    implement encryption key

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,23 +1,28 @@
 const crypto = require('crypto');
 
 const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32); // Schlüssel mit 32 Bytes
-const iv = crypto.randomBytes(16);  // Initialisierungsvektor mit 16 Bytes
 
 // Nachricht verschlüsseln
 function encrypt(text) {
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    const iv = crypto.randomBytes(16); // Generiere einen neuen IV für jede Verschlüsselung
+    const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex'); // Schlüssel aus Umgebungsvariablen laden
+
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    // Der IV wird zusammen mit dem verschlüsselten Text zurückgegeben
     return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 // Nachricht entschlüsseln
 function decrypt(text) {
-    let textParts = text.split(':');
-    let iv = Buffer.from(textParts.shift(), 'hex');
-    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex'); // Schlüssel aus Umgebungsvariablen laden
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
