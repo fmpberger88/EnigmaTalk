7c6e356b56256858300259034b12b2d03b34f254
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-30 14:31:31 +0200

    refactor login routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,13 @@
 // routes/auth.js
 const express = require('express');
-const passport = require('passport');
-const { register, logout, getCurrentUser } = require('../controllers/authController');
+const { register, login, logout, getCurrentUser } = require('../controllers/authController');
 const { isAuthenticated } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-    res.json({ message: 'Login successful', user: req.user });
-});
+router.post('/login', login);
 
 router.post('/logout', logout);
 router.get('/me', isAuthenticated, getCurrentUser);  // Endpunkt für aktuellen Benutzer
